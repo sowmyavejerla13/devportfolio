fc075e13e9057091831edc338db59c4e03d2bb08
sowmyavejerla <sowmyav@ezeees.com>
2026-07-17 21:21:16 +0300

    test vercel

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,29 +1,7 @@
-import Navbar from "@/components/layout/Navbar";
-import Hero from "@/components/sections/Hero";
-import About from "@/components/sections/About";
-import Skills from "@/components/sections/Skills";
-import Experience from "@/components/sections/Experience";
-import PersonalProjects from "@/components/sections/PersonalProjects";
-import Learning from "@/components/sections/Learning";
-import Contact from "@/components/sections/Contact";
-import Footer from "@/components/sections/Footer";
-import Stats from "@/components/sections/Stats";
-import EnterpriseProjects from "@/components/sections/EnterpriseProjects";
-
 export default function Home() {
   return (
-    <main>
-      <Navbar />
-      <Hero />
-      <Stats />
-      <About />
-      <Skills />
-      <Experience />
-      <EnterpriseProjects />
-      <PersonalProjects />
-      <Learning />
-      <Contact />
-      <Footer />
-    </main>
+    <div>
+      <h1>Hello Vercel</h1>
+    </div>
   );
 }
